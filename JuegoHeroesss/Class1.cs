1f4efc39fb098d4240905771f661af273ff2792f
verno <verno@ASUS>
2026-05-18 18:20:22 -0600

    Agregar archivos de proyecto.

---
+++ JuegoHeroesss/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace JuegoHeroesss
+{
+    public class JuegoHeroess
+    {
+        public void IniciarJuego()
+        {
+            Console.WriteLine("¡Bienvenido al juego de héroes!");
+            // Aquí puedes agregar la lógica para iniciar el juego
+        }
+    }
+}
---
+++ LoginPrueba/FrmLogin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPrueba
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.FlatAppearance.BorderSize = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtclave_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtusuario.Clear();
+            txtclave.Clear();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtusuario.Text == "player1" && txtclave.Text == "admin123+")
+            {
+                Frminicio frminicio = new Frminicio();
+                frminicio.FormClosed += (s, args) => this.Show();
+                frminicio.Show();
+            }
+            else
+            {
+                MessageBox.Show("Los datos ingresados son incorrectos. Inténtalo de nuevo.");
+                txtusuario.Clear();
+                txtclave.Clear();
+                txtusuario.Focus();
+            }
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
